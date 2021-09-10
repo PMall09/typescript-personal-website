a92c8c67fbd5c857819626b759fa31d0a2323d0c
Priyanka Mall <87891538+PMall09@users.noreply.github.com>
2021-09-11 00:06:34 +0530

    Update deploy-github.js

--- scripts/deploy-github.js
+++ scripts/deploy-github.js
@@ -1,1 +1,12 @@
+const ghpages = require('gh-pages')
 
+ghpages.publish(
+  'public',
+  {
+    branch: 'master',
+    repo: 'https://github.com/PMall09/PMall09.github.io.git',
+  },
+  () => {
+    console.log('Deploy Completed!')
+  }
+)
