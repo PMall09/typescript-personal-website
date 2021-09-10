92879a457c34bc01f383715d13aedc794a6e2e30
Priyanka Mall <87891538+PMall09@users.noreply.github.com>
2021-09-10 21:45:02 +0530

    Update index.tsx

--- src/components/ui/Icon/index.tsx
+++ src/components/ui/Icon/index.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import {
+  faGlobe,
+  faChartLine,
+  faCode,
   faLaptopCode,
   faDrawPolygon,
   faEdit,
