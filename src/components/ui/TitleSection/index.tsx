fc872808994b33001c2833b354eda11751828ca1
Priyanka Mall <87891538+PMall09@users.noreply.github.com>
2021-09-10 21:39:43 +0530

    Update index.tsx

--- src/components/ui/TitleSection/index.tsx
+++ src/components/ui/TitleSection/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-
 import * as Styled from './styles';
 import { StyledProps } from './styles';
-
 interface Props extends StyledProps {
   title: string;
   subtitle?: string;
 }
-
 const TitleSection: React.FC<Props> = ({ center, title, subtitle }) => (
   <Styled.TitleSection>
     {subtitle && <Styled.SubTitle center={center}>{title}</Styled.SubTitle>}
@@ -15,5 +12,4 @@ const TitleSection: React.FC<Props> = ({ center, title, subtitle }) => (
     <Styled.Separator center={center} />
   </Styled.TitleSection>
 );
-
 export default TitleSection;
